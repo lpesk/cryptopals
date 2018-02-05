4276eb0fa07c6f7b4f45224b778413b56550ed50
Laura Peskin <laura.peskin@gmail.com>
2018-02-05 15:04:18 -0800

    manually deleted files

--- challenges/set1ch5.py
+++
@@ -1,9 +0,0 @@
-#####################################################
-######     cryptopals set 1, challenge 5       ######
-#####################################################
-
-########## implement repeating-key XOR  #############
-
-from tools import repXOR, encode
-
-print encode(repXOR('Burning \'em, if you ain\'t quick and nimble\nI go crazy when I hear a cymbal', 'ICE'), 'hex')
--- tools/repxor.py
+++
@@ -1,7 +0,0 @@
-from bit_operations import XOR
-
-def repXOR(msg, key):
-    assert(len(key) > 0), "Key must contain at least 1 byte"
-    key_rem = key[0:(len(msg) % len(key))]
-    key_rep = key * int(len(msg)/len(key)) + key_rem
-    return XOR(msg, key_rep)
