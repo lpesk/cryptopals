0869bd6dcb2e5412cee5f1c4a85de063a7561509
Laura Peskin <laura.peskin@gmail.com>
2018-02-06 14:07:17 -0800

    add test for challenge 1

--- challenge01.py
+++ challenge01.py
@@ -1,9 +1,5 @@
 from tools.message import Message
+import pytest
 
-in_str = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-out_str = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-
-msg_b64 = Message(in_str, 'hex').base64()
-assert (msg_b64 == out_str)
-
-
+def hexToBase64(msg_hex):
+    return Message(msg_hex, 'hex').base64()
